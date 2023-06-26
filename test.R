19dddcdcea02905144ca769cf45a5da9d909e695
sokuroda <135292562+sokuroda@users.noreply.github.com>
2023-06-27 00:36:23 +0900

    局がある場合の処理(1パターン)に絞ってコードを書いてみました。
    今回は、関数との連携もとることができました。
    ただ、まだ局の処理に関しましても未完成なので明日続きを行いたいと考えております。
    明日も宜しくお願い致します。

--- test.R
+++ test.R
@@ -6,6 +6,10 @@ library(stringr)
 path <- file.choose()
 df <- read.csv(path,fileEncoding = "Shift-jis",colClasses = "character")
 
+#関数の受け入れ
+#path_function <- file.choose()
+#source(path_function)
+
 #加工すべき変数(「区分中で、"局"があれば無くす」をとりあえず行う)
 condition <- (
   str_detect(df$品名,"局"))
@@ -14,7 +18,8 @@ condition <- (
 to_engineer <- df %>% filter(condition) %>% nrow()
 not_to_engineer <- df %>% filter(!condition) %>% nrow()
 
-num_before_row(to_engineer)
+#「加工すべき変数」に引っかかったものを関数により処理する
+engineer(to_engineer)
 
-
-num_whole <- df %>% filter(新品名!=品名) %>% nrow()
+#
+num_whole <- df_enginered %>% filter(new_stuff!=品名) %>% nrow()
