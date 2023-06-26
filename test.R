60a9250842647f5137db71bf5db865a0f3b5df1e
sokuroda <135292562+sokuroda@users.noreply.github.com>
2023-06-26 18:27:17 +0900

    一部修正しました。

--- test.R
+++ test.R
@@ -1,17 +1,20 @@
+library(tidyverse)
+library(stringr)
+
 #test
-path <- "C:\\Users\\sokur\\OneDrive\\デスクトップ\\2023_06_16.csv"
-df <- read_csv(path, locale = locale(encoding = "shift-jis"))
+#データ入力
+path <- file.choose()
+df <- read.csv(path,fileEncoding = "Shift-jis",colClasses = "character")
 
-
-#加工すべき変数と加工
+#加工すべき変数(「区分中で、"局"があれば無くす」をとりあえず行う)
 condition <- (
-  str_detect()|
-    str_ditect()
-)
-to_engineer <- df %>% filter(conditon)
-not_to_engineer <- df %>% filter(!conditon)
+  str_detect(df$品名,"局"))
+
+#加工を加えるべきもの(to_engineer)と加えなくてよいもの(not_to_enginner)
+to_engineer <- df %>% filter(condition) %>% nrow()
+not_to_engineer <- df %>% filter(!condition) %>% nrow()
 
 num_before_row(to_engineer)
 
 
-num_whole <- df %>% filter(新品名!=品名) %>% NROW
+num_whole <- df %>% filter(新品名!=品名) %>% nrow()
