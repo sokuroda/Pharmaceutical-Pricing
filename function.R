da808697885696071d24072905bfc79dc61c35b0
sokuroda <135292562+sokuroda@users.noreply.github.com>
2023-06-28 15:29:26 +0900

    "　局"の処理関数
    遅くなってしまい申し訳ございません。
    昨日の作成分を更新致します。

--- function.R
+++ function.R
@@ -1,7 +1,23 @@
 #正しい品名を得る
 #df_engineredがその比較表
 #function 
-engineer <- function(var){
-  df_enginered <- mutate(df,
-                         new_stuff = str_replace(var,"局",""))
+#「　局」→131
+#「局　」→2600
+#「局※ 　」→307
+#「局麻　」→27
+#「（局）」→206
+
+#to_engineeredに対して、加工し新品名を作成する関数
+engineer <- function(df){
+  df_engineered <- mutate(df,新品名=str_replace(品名,"局　",""))
+    return(df_engineered)
 }
+    #mutate(df,
+                          #new_stuff = str_replace(var,"　局","")|
+                          #  str_replace(var,"局　","")|
+                           # str_replace(var,"局※　","")|
+                            #str_replace(var,"局麻　","")|
+                            #str_replace(var,"（局）",""))
+#}
+
+
